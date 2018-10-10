9f6c7ffe6905b7dfa185a0e765f767c15fde2425
R.beckson <becksonq@gmail.com>
2018-10-10 15:48:07 +0300

    agency-client init

---
+++ src/Domain/V2/AgencyClientSettings.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Koma136\MyTarget\Domain\V2;
+
+
+class AgencyClientSettings
+{
+    /**
+     * @var string
+     * @Field(name="access_type", type="string")
+     */
+    private $access_type;
+    
+    /**
+     * @var string
+     * @Field(name="status", type="string")
+     */
+    private $status;
+    
+    /**
+     * @var UserClient
+     */
+    private $user;
+    
+    /**
+     * @return string
+     */
+    public function getAccessType()
+    {
+        return $this->access_type;
+    }
+    
+    /**
+     * @param $accessType
+     * @return mixed
+     */
+    public function setAccessType($accessType)
+    {
+        return $this->access_type = $accessType;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    
+    /**
+     * @param $status
+     * @return mixed
+     */
+    public function setStatus($status)
+    {
+        return $this->status = $status;
+    }
+    
+    /**
+     * @return UserClient
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+    
+    /**
+     * @param UserClient $user
+     * @return UserClient
+     */
+    public function setUser(UserClient $user)
+    {
+        return $this->user = $user;
+    }
+}
